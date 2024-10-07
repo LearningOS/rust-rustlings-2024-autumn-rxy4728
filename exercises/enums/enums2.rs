dbbcd8aecc612ee174e6b97b26b2b0d2d9f137a2
rxy4728 <flynnu4728@outlook.com>
2024-10-07 22:19:59 +0800

    update

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,15 +3,13 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
-    Move:{i32,i32},
-    Echo:String,
-    ChangeColor:(i32,i32,i32),
-    Quit
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 impl Message {
